6771e07dc4c5e47dd595a8a0c4c872137d3e8143
TahaAliAbbasi <163841832+TahaAliAbbasi@users.noreply.github.com>
2024-03-23 12:04:14 -0700

    Added some text in message and added console at the end of code

--- index.ts
+++ index.ts
@@ -3,8 +3,8 @@
 import inquirer from "inquirer";
 
 const answer = await inquirer.prompt([
-{ message: "Enter your first number", type: "number", name: "firstNumber" },
-{message: "Enter your second number", type: "number", name: "secondNumber"},
+{ message: "Enter your first number here", type: "number", name: "firstNumber" },
+{message: "Enter your second number here", type: "number", name: "secondNumber"},
 
 {
     message: "select one of the operators to perform operation",
